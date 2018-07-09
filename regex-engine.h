bc47626685c44d123e46cfbbdd34b392a37d84cf
Phantomape <cxucheng@outlook.com>
2018-07-08 23:55:47 -0700

    Add regex engine header

---
+++ regex-engine.h
@@ -0,0 +1,18 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <regex.h>
+
+/* Compile regex string pattern to a regex_t-array. */
+regex_t* re_compile(const char *pattern);
+
+/* Find matches of the compiled pattern inside text. */
+int re_matchp(regex_t *pattern, const char *text);
+
+/* Find matches of the txt pattern inside text (will compile automatically first). */
+int re_match(const char* pattern, const char* text);
+
+#ifdef __cplusplus
+}
+#endif
